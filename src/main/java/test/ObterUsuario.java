1a107e8d1fc27ffaff1487bd7739cbe851b68ab4
João Vieira <mutantinotrue@outlook.com>
2025-11-04 12:51:31 -0300

    Consultando Usuario

---
+++ src/main/java/test/ObterUsuario.java
@@ -0,0 +1,20 @@
+package test;
+
+import model.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ObterUsuario {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("curso-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        Usuario usuario = em.find(Usuario.class, 1L);
+        System.out.println(usuario.getNome());
+
+        em.close();
+        emf.close();
+    }
+}
